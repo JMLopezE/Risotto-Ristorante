285a80d42f4ffc7199886511e797137ca32e8fa5
rafaelgm08 <rafael000182@gmail.com>
2023-12-11 18:47:31 -0500

    adicion footer

--- Risotto-Ristorante/src/app/app.component.ts
+++ Risotto-Ristorante/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './page/footer/footer.component';
+import { FormulariofooterComponent } from './page/formulariofooter/formulariofooter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent, FormulariofooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
