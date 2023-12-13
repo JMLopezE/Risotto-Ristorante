e0a7c642d75ed33a55c434f498ddb5d8d62acd97
arelis bonilla <afroditazetax@hotmail.com>
2023-12-13 12:58:33 -0500

    cambio arelis

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MenuComponent } from './PAGES/menu/menu.component';
 import { MenuPastasComponent } from './PAGES/menu-pastas/menu-pastas.component';
+import { MenuBebidasComponent } from './PAGES/menu-bebidas/menu-bebidas.component';
 
 export const routes: Routes = [
         { path: "menu", component: MenuComponent},
-        { path: "menu-pastas", component: MenuPastasComponent}
+        { path: "menu-pastas", component: MenuPastasComponent},
+        { path: "menu-bebidas", component: MenuBebidasComponent}
 ];
