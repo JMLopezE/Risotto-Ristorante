8b40f37073cc36a36f3ea687a941f3ba128b2aa3
ALVARO08-TIGER <venon_men@yahoo.es>
2023-12-15 17:49:34 -0500

    creacion del nav y main

---
+++ src/app/page/nav-main/nav-main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-nav-main',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './nav-main.component.html',
+  styleUrl: './nav-main.component.css'
+})
+export class NavMainComponent {
+
+}
