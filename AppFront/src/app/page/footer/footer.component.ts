08c0b9a0e05af36c5ae1d2bdedfe5707076b4a3c
JMLopezE <julylopez02@gmail.com>
2023-12-19 20:00:26 -0500

    cambios

--- AppFront/src/app/page/footer/footer.component.ts
+++ AppFront/src/app/page/footer/footer.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
+
 export class FooterComponent {
-  @Input() title:String = "soy el titulo"
+  @Input() title: String = 'soy el titulo';
 }
