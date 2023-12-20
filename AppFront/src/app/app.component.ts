00419e7d06544f6378db265ed3424ca5c8e383b1
JMLopezE <julylopez02@gmail.com>
2023-12-20 13:05:44 -0500

    Ajustes nav

--- AppFront/src/app/app.component.ts
+++ AppFront/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './page/footer/footer.component';
 import { FormulariofooterComponent } from './page/formulariofooter/formulariofooter.component';
 import { NavMainComponent } from './page/nav-main/nav-main.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, FormulariofooterComponent,NavMainComponent, RouterModule],
+  imports: [CommonModule, RouterOutlet, FooterComponent, FormulariofooterComponent,NavMainComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- AppFront/src/app/page/footer/footer.component.ts
+++ AppFront/src/app/page/footer/footer.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css',
+  styleUrl: './footer.component.css'
 })
-
 export class FooterComponent {
-  @Input() title: String = 'soy el titulo';
+  @Input() title:String = "soy el titulo"
 }
